9734d7de3a7208625d38bdc8496b643a362368c0
Pedro Alves <pedrojunior20132013@gmail.com>
2021-11-04 22:36:07 -0300

    correção

--- mult/index.js
+++ mult/index.js
@@ -1,32 +1,20 @@
-// const express = require('express');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const port = 80;
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// app.get('/multiplicacao/:num1/:num2', (req, res) => {
-//     const result  = {
-//         res : req.params.num1 * req.params.num2
-//     }
-//     res.send(result);
-//     console.log(result);
-// });
-
-// console.log(`Aplicação rodando na porta ${port}`);
-// app.listen(port);
-
-const express = require('express')
-const HOST = '0.0.0.0'
-const PORT = 80
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 80;
 
 const app = express();
-app.get('/mult', (req,res) => {
-    res.send('jesus te ama');
-    console.log('alo');
-})
+app.use(bodyParser.json());
 
-app.listen(PORT,HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+ app.get('/multiplicacao/:num1/:num2', (req, res) => {
+    const result  = {
+         res : req.params.num1 * req.params.num2
+    }
+     res.send(result);
+     console.log(result);
+});
+
+ console.log(`Aplicação rodando na porta ${port}`);
+ app.listen(port);
+
 
